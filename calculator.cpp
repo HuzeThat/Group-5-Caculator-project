a106d2a41575ba693e77422588319aab9861b0d7
Huzeifa <nesinasuha@gmail.com>
2025-04-06 13:57:32 +0300

    implement interface

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,36 @@ using namespace std;
 int subtract(int a, int b);
 int divide(int a, int b);
 int main() {
+    int num1, num2;
+ char operation;
+ cout << "Enter two numbers: ";
+ cin >> num1 >> num2;
+ cout << "Choose operation (+, -, *, /): ";
+ cin >> operation;
+ switch (operation) {
+ case '+':
+ cout << "Result: " << add(num1, num2) << endl;
+ break;
+ case '-':
+ cout << "Result: " << subtract(num1, num2) << endl;
+ break;
+ case '*':
+ cout << "Result: " << multiply(num1, num2) << endl;
+ break;
+ case '/':
+ cout << "Result: " << divide(num1, num2) << endl;
+ break;
+ default:
+ cout << "Invalid operation!" << endl;
+ }
+
+
+
+
+
+
+
+
 
 
 
